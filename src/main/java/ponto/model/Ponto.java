910736f59180f0db77b3eea95900f73dbfebbff3
viniciusbellini <viniciusbellini@gmail.com>
2014-10-27 23:37:54 -0200

    1

---
+++ src/main/java/ponto/model/Ponto.java
@@ -0,0 +1,14 @@
+package ponto.model;
+
+/**
+ * Created by Vinicius on 27/10/2014.
+ */
+public class Ponto {
+
+    //Atributos
+    private String data;
+    private String horaEntrada;
+    private String horaSaida;
+
+
+}
